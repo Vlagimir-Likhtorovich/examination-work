9db855b6272644ab6cc74038236ee9385356e067
Vova Likhtarovich <nsctptc2g@gmail.com>
2023-03-11 22:48:40 +0300

    removing duplicate styles in markup

---
+++ script_token.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".open-menu").addEventListener("click", function () {
+        document.querySelector(".mobil-menu").classList.toggle("hidden");
+    });
+
+    document.body.addEventListener("click", function (e) {
+        if (!e.target.closest(".open-menu")) {
+            document.querySelector(".mobil-menu").classList.add("hidden");
+        }
+    });
+});
