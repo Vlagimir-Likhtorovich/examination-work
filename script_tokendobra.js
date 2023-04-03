a24329fe31b8a2614d65bcbabf3c8b7cc999a25b
Vova Likhtarovich <nsctptc2g@gmail.com>
2023-04-04 00:53:40 +0300

    modal window script

--- script_tokendobra.js
+++ script_tokendobra.js
@@ -27,25 +27,6 @@ window.addEventListener("DOMContentLoaded", function () {
         
         
     });
-    document.querySelector(".button-header-raccoon").addEventListener("click", function(event){
-        event.preventDefault();
-        document.querySelector(".modal-overlay").classList.remove("hidden");
-    
-        setTimeout(function(){
-            document.querySelector(".modal-content").classList.add("ready");
-        });
-    
-    
-    });
-    
-    document.querySelector(".close-menu-modal-btn").addEventListener("click", function(event){
-        document.querySelector(".modal-content").classList.remove("ready");
-        setTimeout(function(){
-            document.querySelector(".modal-overlay").classList.add("hidden");
-        }, 200);
-        
-        
-    });
     document.querySelector(".button-header-mobil").addEventListener("click", function(event){
         event.preventDefault();
         document.querySelector(".modal-overlay").classList.remove("hidden");
@@ -65,6 +46,28 @@ window.addEventListener("DOMContentLoaded", function () {
         
         
     });
+
+
+    document.querySelector(".button-header-raccoon").addEventListener("click", function(event){
+        event.preventDefault();
+        document.querySelector(".modal-overlay").classList.remove("hidden");
+    
+        setTimeout(function(){
+            document.querySelector(".modal-content").classList.add("ready");
+        });
+    
+    
+    });
+    
+    document.querySelector(".close-menu-modal-btn").addEventListener("click", function(event){
+        document.querySelector(".modal-content").classList.remove("ready");
+        setTimeout(function(){
+            document.querySelector(".modal-overlay").classList.add("hidden");
+        }, 200);
+        
+        
+    });
+   
     
     
     
